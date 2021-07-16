7d8b4d3c2a71ddc7f3eb8f569e587fb9dab062d1
GoodDonkey <president20500@gmail.com>
2021-07-16 18:31:24 +0900

    develop branch

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app =  Flask(__name__)
 def index_view():
     return render_template("index.html")
 
+
+@app.route("/test", methods=["GET"])
+def index_view():
+    return render_template("test.html")
+
 if __name__=="__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
