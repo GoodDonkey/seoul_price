423553f1243c8824e8b40c6ab1fe202bdf8dd74d
mechaniclib <ymk092792@gmail.com>
2021-07-19 18:19:45 +0900

    last

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app =  Flask(__name__)
 def index_view():
     return render_template("index.html")
 
-
-@app.route("/graph", methods=["GET"])
-def show_graph():
-    return 'anydata'
+@app.route("/showIframe", methods=["GET"])
+def iframe():
+    data =''
+    return data
 
 if __name__=="__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
