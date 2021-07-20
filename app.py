72a7ed0fcfba62044327f29ffa384acf72a0ed76
mechaniclib <ymk092792@gmail.com>
2021-07-20 14:35:03 +0900

    iframe

--- app.py
+++ app.py
@@ -6,6 +6,22 @@ app =  Flask(__name__)
 def index_view():
     return render_template("index.html")
 
+@app.route("/iframe01", methods=["POST"])
+def iframe01():
+    return render_template("index.html")
+
+@app.route("/iframe02", methods=["POST"])
+def iframe02():
+    return render_template("index.html")
+
+@app.route("/iframe03", methods=["POST"])
+def iframe03():
+    return render_template("index.html")
+
+@app.route("/iframe04", methods=["POST"])
+def iframe04():
+    return render_template("index.html")
+
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,61 @@
-function importIframe() {
-    xhttp.open("GET", "showIframe");
-    xhttp.send();
+function iframe01() {
     const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "iframe01");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
         xhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("iframe").style.display = "block";
+                document.getElementById("iframe02").style.display = "none";
+                document.getElementById("iframe03").style.display = "none";
+                document.getElementById("iframe04").style.display = "none";
+                document.getElementById("iframe01").style.display = "block";
 
             }
         }    
+}
+
+function iframe02() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "iframe02");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe01").style.display = "none";
+            document.getElementById("iframe03").style.display = "none";
+            document.getElementById("iframe04").style.display = "none";
+            document.getElementById("iframe02").style.display = "block";
+        }
+    }    
+}
+
+function iframe03() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "iframe03");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe01").style.display = "none";
+            document.getElementById("iframe02").style.display = "none";
+            document.getElementById("iframe04").style.display = "none";
+            document.getElementById("iframe03").style.display = "block";
+            
+        }
+    }    
+}
+
+function iframe04() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "iframe04");
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe01").style.display = "none";
+            document.getElementById("iframe02").style.display = "none";
+            document.getElementById("iframe03").style.display = "none";
+            document.getElementById("iframe04").style.display = "block";
+        }
+    }    
 }
