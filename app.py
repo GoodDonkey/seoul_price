3606a1d68b82ce2481b3494a5e6e2d47fbb45a82
GoodDonkey <president20500@gmail.com>
2021-07-20 14:37:43 +0900

    dk button edit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 
 app =  Flask(__name__)
 
@@ -6,14 +6,30 @@ app =  Flask(__name__)
 def index_view():
     return render_template("index.html")
 
-@app.route("/showIframe", methods=["GET"])
-def iframe():
-    data =''
-    return data
 
 @app.route("/login", methods=["GET"])
 def login():
     return render_template("login.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login_check():
+    user_id = request.form.get('id')
+    user_pw = request.form.get('pw')
+    print("받은id는------------------",user_id)
+    print("받은pw는------------------",user_pw)
+    if user_id == 'donkey' and user_pw == 'ehdrl123':
+        return redirect(url_for('index_view'))
+    return render_template("login.html")
+
+
+@app.route("/showContents01", methods=["GET", "POST"])
+def showContents01():
+    return 'anydata'
+
+@app.route("/showIframe", methods=["GET"])
+def iframe():
+    data =''
+    return data
+    
 if __name__=="__main__":
     app.run(debug=True, host="127.0.0.1", port=5000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,56 @@
+function showContents01() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "showContents01");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("contents_div").style.display = "block";
+
+        };
+    };  
+};
+
 
 function importIframe() {
+    const xhttp = new XMLHttpRequest();
     xhttp.open("GET", "showIframe");
     xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe_div").style.display = "block";
+            document.getElementById("iframe_div").innerHTML = `<iframe src="http://localhost:5601/app/visualize#/edit/491da7b0-e604-11eb-850e-0f116349053a?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="100%" width="100%"></iframe>`
+
+        }
+    }    
+}
+
+// 하나의 함수로 쓸 수 있을거 같은데 일단 테스트 용으로 따로 만들어봄
+function importIframe2() {
     const xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("iframe_div").innerHTML = this.responseText;
-            }
-        }    
+    xhttp.open("GET", "showIframe");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe_div2").style.display = "block";
+            document.getElementById("iframe_div2").innerHTML = `<iframe src="http://localhost:5601/app/visualize#/edit/491da7b0-e604-11eb-850e-0f116349053a?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="100%" width="100%"></iframe>`
+
+        }
+    }    
+}
+
+function importIframe3() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "showIframe");
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe_div3").style.display = "block";
+            document.getElementById("iframe_div3").innerHTML = `<iframe src="http://localhost:5601/app/visualize#/edit/491da7b0-e604-11eb-850e-0f116349053a?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="100%" width="100%"></iframe>`
+
+        }
+    }    
 }
