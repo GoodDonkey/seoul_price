731dc6d086cfcf3a2f9acd8a44462ef6af0d4eff
mechaniclib <ymk092792@gmail.com>
2021-07-20 17:46:27 +0900

    template

--- static/js/script.js
+++ static/js/script.js
@@ -25,7 +25,7 @@ function iframe02() {
             document.getElementById("iframe01").style.display = "none";
             document.getElementById("iframe03").style.display = "none";
             document.getElementById("iframe04").style.display = "none";
-            document.getElementById("iframe02").innerHTML = `<iframe src="http://localhost:5601/goto/875a26a1aed54c975c53dd6b3b291a51" height="800" width="1600"></iframe>`;
+            document.getElementById("iframe02").innerHTML = `<iframe src="http://localhost:5601/goto/875a26a1aed54c975c53dd6b3b291a51" height="900" width="1600"></iframe>`;
             document.getElementById("iframe02").style.display = "block";
         }
     }
@@ -41,7 +41,7 @@ function iframe03() {
             document.getElementById("iframe01").style.display = "none";
             document.getElementById("iframe02").style.display = "none";
             document.getElementById("iframe04").style.display = "none";
-            document.getElementById("iframe03").innerHTML = `<iframe src="http://localhost:5601/goto/73b5c561e5c7647c187920d7034b950d" height="800" width="1600"></iframe>`;
+            document.getElementById("iframe03").innerHTML = `<iframe src="http://localhost:5601/goto/73b5c561e5c7647c187920d7034b950d" height="900" width="1600"></iframe>`;
             document.getElementById("iframe03").style.display = "block";
         }
     }
@@ -57,7 +57,7 @@ function iframe04() {
             document.getElementById("iframe01").style.display = "none";
             document.getElementById("iframe02").style.display = "none";
             document.getElementById("iframe03").style.display = "none";
-            document.getElementById("iframe04").innerHTML = `<iframe src="http://localhost:5601/goto/0155f600e3de6e0c35a65a39126bce31" height="800" width="1600"></iframe>`;
+            document.getElementById("iframe04").innerHTML = `<iframe src="http://localhost:5601/goto/0155f600e3de6e0c35a65a39126bce31" height="900" width="1600"></iframe>`;
             document.getElementById("iframe04").style.display = "block";
         }
     }
