8d223afbd0b78c56ec05b654e9cb8a448f32c78c
mechaniclib <ymk092792@gmail.com>
2021-07-20 17:01:37 +0900

    template

--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,16 @@ function iframe01() {
     xhttp.open("POST", "iframe01");
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("iframe02").style.display = "none";
-                document.getElementById("iframe03").style.display = "none";
-                document.getElementById("iframe04").style.display = "none";
-                document.getElementById("iframe01").innerHTML = ``;
-                document.getElementById("iframe01").style.display = "block";
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("iframe02").style.display = "none";
+            document.getElementById("iframe03").style.display = "none";
+            document.getElementById("iframe04").style.display = "none";
+            document.getElementById("iframe01").innerHTML = ``;
+            document.getElementById("iframe01").style.display = "block";
 
-            }
-        }    
+        }
+    }
 }
 
 function iframe02() {
@@ -28,7 +28,7 @@ function iframe02() {
             document.getElementById("iframe02").innerHTML = `<iframe src="http://localhost:5601/goto/875a26a1aed54c975c53dd6b3b291a51" height="800" width="1600"></iframe>`;
             document.getElementById("iframe02").style.display = "block";
         }
-    }    
+    }
 }
 
 function iframe03() {
@@ -44,7 +44,7 @@ function iframe03() {
             document.getElementById("iframe03").innerHTML = `<iframe src="http://localhost:5601/goto/73b5c561e5c7647c187920d7034b950d" height="800" width="1600"></iframe>`;
             document.getElementById("iframe03").style.display = "block";
         }
-    }    
+    }
 }
 
 function iframe04() {
@@ -60,5 +60,5 @@ function iframe04() {
             document.getElementById("iframe04").innerHTML = `<iframe src="http://localhost:5601/goto/0155f600e3de6e0c35a65a39126bce31" height="800" width="1600"></iframe>`;
             document.getElementById("iframe04").style.display = "block";
         }
-    }    
+    }
 }
